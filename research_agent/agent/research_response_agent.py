f51f9dfa579aaf0cb4d1b4230de4f7ca271aefb6
Bjørn Göttler <bjoern@goettler.dk>
2025-05-07 11:25:01 +0200

    agents: Add research agent

--- research_agent/agent/research_response_agent.py
+++ research_agent/agent/research_response_agent.py
@@ -1,4 +1,3 @@
-import argparse
 from typing import Final
 from autogen import ConversableAgent
 from research_agent.tools.query_tool import query_scholar
@@ -28,12 +27,13 @@ def create_research_agent() -> ConversableAgent:
                     You will ask the user if the year should be before, after or the exact year of publication
                     You can query the Semantic Scholar API using the query_scholar tool.
                     You can read the API response and return it
-                    Don't include any other text in your response.""",
+                    Don't include any other text in your response.
+                    Return 'TERMINATE' when the task is done.""",
         llm_config=LLM_CONFIG,
     )
 
     # add the tools to the agent
-    agent.register_for_llm(name="query_scholar", description="Read customer feedback")(query_scholar)
+    agent.register_for_llm(name="query_scholar", description="Read query response")(query_scholar)
 
     return agent
 
@@ -42,35 +42,20 @@ def create_user_proxy():
         name="User",
         llm_config=False,
         is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
-        human_input_mode="NEVER",
+        human_input_mode="ALWAYS",
     )
-    user_proxy.register_for_execution(name="feedback_reader")(query_feedback)
-    user_proxy.register_for_execution(name="sentiment_analysis")(analyze_sentiment)
-    user_proxy.register_for_execution(name="categorization")(categorize_feedback)
-    user_proxy.register_for_execution(name="keyword_extraction")(extract_keywords)
+    user_proxy.register_for_execution(name="query_scholar")(query_scholar)
     return user_proxy
 
 
-    def main():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--mode", choices=["sentiment_analysis", "keyword_extraction", "categorization", "categorize_and_extract_keywords"], required=True, default="sentiment_analysis")
-        args = parser.parse_args()
+def main():
+    prompt = PROMPT_RESEARCH_QUERY
+    research_agent = create_research_agent()
+    user_proxy = create_user_proxy()
 
-        prompt = None
-        if args.mode == "sentiment_analysis":
-            prompt = PROMPT_SENTIMENT_ANALYSIS
-        elif args.mode == "keyword_extraction":
-            prompt = PROMPT_KEYWORD_EXTRACTION
-        elif args.mode == "categorization":
-            prompt = PROMPT_CATEGORIZATION
-        elif args.mode == "categorize_and_extract_keywords":
-            prompt = PROMPT_CATEGORIZE_AND_EXTRACT_KEYWORDS
-
-        user_proxy = create_user_proxy()
-        feedback_analysis_agent = create_feedback_analysis_agent()
-        user_proxy.initiate_chat(
-            feedback_analysis_agent,
-            message=prompt
+    user_proxy.initiate_chat(
+        research_agent,
+        message=prompt
         )
 
     if __name__ == "__main__":
