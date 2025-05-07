7d6ba8278b8b7fa69bbce2589d40dfea75ea24c1
Bjørn Göttler <bjoern@goettler.dk>
2025-05-07 14:43:28 +0200

    agents: add logging

--- research_agent/agent/research_response_agent.py
+++ research_agent/agent/research_response_agent.py
@@ -1,8 +1,14 @@
+import logging
 from copy import deepcopy
 from typing import Final
 from autogen import ConversableAgent
 from research_agent.tools.query_tool import query_scholar
 from research_agent.config import LLM_CONFIG
+from research_agent.logging_configuration import setup_logging
+
+setup_logging(log_file="research_agent.log")
+
+logger = logging.getLogger(__name__)
 
 
 
@@ -24,6 +30,7 @@ Format: "{MANDATORY_QUERY}"
 
 def create_research_agent() -> ConversableAgent:
     # define the agent
+    logger.debug("Creating research agent")
     config = deepcopy(LLM_CONFIG)
     if "functions" not in config:
         config["functions"] = []
@@ -41,6 +48,7 @@ def create_research_agent() -> ConversableAgent:
             "required": ["param1", "param2", "param3"],
         }
     })
+    logger.debug(f"config: {config}")
 
     agent = ConversableAgent(
         name="Research Agent",
@@ -60,11 +68,13 @@ def create_research_agent() -> ConversableAgent:
 
     # add the tools to the agent
     agent.register_for_llm(name="query_scholar", description="Search for academic papers")(query_scholar)
+    logger.info("Research agent created")
 
     return agent
 
 
 def create_user_proxy():
+    logger.debug("Creating user proxy")
     user_proxy = ConversableAgent(
         name="User",
         llm_config=False,
@@ -72,14 +82,17 @@ def create_user_proxy():
         human_input_mode="ALWAYS",
     )
     #user_proxy.register_for_execution(name="query_scholar")(query_scholar)
+    logger.info("User proxy created")
     return user_proxy
 
 
 def main():
+    logger.info("Starting research agent")
     prompt = PROMPT_RESEARCH_QUERY
     research_agent = create_research_agent()
     user_proxy = create_user_proxy()
 
+    logger.info("Starting chat")
     user_proxy.initiate_chat(
          research_agent,
          message=prompt
@@ -87,4 +100,5 @@ def main():
 
 
 if __name__ == "__main__":
+    logger.info("Going off!")
     main()
--- research_agent/tools/query_tool.py
+++ research_agent/tools/query_tool.py
@@ -1,5 +1,9 @@
+import logging
 from research_agent.tools import semanticscholar
 
+logger = logging.getLogger(__name__)
+
 def query_scholar(search_string: str, year: str, minCitationCount: str) -> str:
+    logger.debug(f"Searching for {search_string} in {year} with minCitationCount {minCitationCount}")
     search_results = semanticscholar.get_semantic_search(search_string, year, minCitationCount)
     return search_results
